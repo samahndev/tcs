00f22c546e79d2570b0bf289d69d1a7d02135430
Sam Ahn <sam.ahn.dev@gmail.com>
2018-09-17 23:45:34 +1200

    chore(ui): philosophy layout

--- src/components/philosophy/index.js
+++ src/components/philosophy/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default () => (
   <div className="philosophy">
-    <h1>A place to come and treat yourself</h1>
-    <p>
-      Tortor tenetur viverra nonummy irure. Excepturi arcu inventore quibusdam magnis quo soluta fugit.
+    <h1 className="philosophy-heading">A place to come and treat yourself</h1>
+    <p className="philosophy-copy">
+      Tortor tenetur viverra nonummy irure. Excepturi arcu inventore quibusdam magnis quo soluta fugit. Tortor tenetur viverra nonummy irure.<br />Excepturi arcu inventore quibusdam magnis quo soluta fugit.
     </p>
   </div>
 );
