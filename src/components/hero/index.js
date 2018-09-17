71d7fc2b6957db86adf95fca93e46b2267a35041
Sam Ahn <sam.ahn.dev@gmail.com>
2018-09-17 17:28:08 +1200

    chore(ui): hero layout

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -3,17 +3,17 @@ import React from 'react';
 export default () => (
   <div className="hero">
     <div className="hero-content">
-      <h1>
+      <h1 className="hero-content-title">
         All day eatery,
         <br />
         Kiwi food with a twist.
       </h1>
-      <p>
-        2 -3 Osbourne St.
+      <p className="hero-content-address">
+        2-8 Osbourne Street,
         <br />
         Newmarket, Auckland
       </p>
-      <p>
+      <p className="hero-content-opening-hours">
         Monday - Friday | 7AM-10PM Saturday & Sunday | 8AM-10PM
       </p>
     </div>
