ed63b74c19816df8e07c55846d03382573ebec0d
samahndev <sam.ahn.dev@gmail.com>
2019-07-20 03:22:24 +1200

    chore: hero animation

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,23 +1,58 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade'
+import * as SVGLoaders from 'svg-loaders-react';
 
-export default () => (
-  <div className="hero">
-    <div className="hero-content">
-      <h1 className="hero-content-title">
-        All day eatery,
-        <br />
-        Kiwi food with a twist
-      </h1>
-      <p className="hero-content-address">
-        2-8 Osbourne Street,
-        <br />
-        Newmarket, Auckland
-      </p>
-      <p className="hero-content-opening-hours">
-        Monday <span>/</span> 7am <span>-</span> 4am<br />
-        Tuesday <span>-</span> Friday <span>/</span> 7am <span>-</span> 9pm<br />
-        Saturday <span>/</span> 8am <span>-</span> 9pm<span>, </span>Sunday <span>/</span> 8am <span>-</span> 4pm
-      </p>
-    </div>
-  </div>
-);
+import PreloadImage from '../preload-image'
+import heroImage from '../../images/petr-sevcovic-594807-unsplash.jpg';
+
+export default class Hero extends React.Component {
+  state = { heroLoaded: false }
+  render() {
+    return (
+      <div className="hero">
+        <PreloadImage
+          className="heroImage"
+          src={heroImage}
+          loadSuccess={() => {
+            setTimeout(() => {
+              this.setState({ heroLoaded: true })
+            }, 1000)
+          }}
+        />
+        <div className="hero-content">
+          {this.state.heroLoaded ?
+            <div>
+              <h1 className="hero-content-title">
+                <Fade bottom cascade>
+                  <span>All day eatery,</span>
+                </Fade>
+                <br />
+                <Fade bottom cascade>
+                  <span>Kiwi food with a twist</span>
+                </Fade>
+              </h1>
+              <Fade bottom>
+                <div>
+                  <p className="hero-content-address">
+                    2-8 Osbourne Street,
+                    <br />
+                    Newmarket, Auckland
+                  </p>
+                  <p className="hero-content-opening-hours">
+                    Monday <span>/</span> 7am <span>-</span> 4am<br />
+                    Tuesday <span>-</span> Friday <span>/</span> 7am <span>-</span> 9pm<br />
+                    Saturday <span>/</span> 8am <span>-</span> 9pm<span>, </span>Sunday <span>/</span> 8am <span>-</span> 4pm
+                  </p>
+                </div>
+              </Fade>
+            </div>
+            :
+            <div className="hero-loader">
+              <SVGLoaders.TailSpin />
+            </div>
+          }
+        </div>
+      </div>
+    )
+  }
+}
